65ec2be89f591a02db9fb86a66357821b38b6258
Oleg Slobodskoi <oleg008@gmail.com>
2016-08-15 00:38:16 +0200

    first version

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import {create} from 'jss'
+import preset from 'jss-preset-default'
+
+export default function aphrodisiac(jss, options = {}) {
+  const sheet = jss
+    .createStyleSheet(null, {meta: 'aphrodite-jss', ...options})
+    .attach()
+
+  const isNotEmpty = val => val != null
+
+  const getClassName = rule => rule.className
+
+  function css(...rules) {
+    // Filter out falsy values from the input, to allow for
+    // `css(a, test && c)`
+    rules = rules.filter(isNotEmpty)
+
+    if (!rules.length) return ''
+
+    // A compound class name from all rules.
+    const className = rules.map(getClassName).join('--')
+
+    if (sheet.getRule(className)) return className
+
+    const style = rules.reduce((res, rule) => Object.assign(res, rule.style), {})
+
+    // TODO make jss accept options here.
+    sheet.addRule(className, style, {className})
+
+    return className
+  }
+
+  function register(styles) {
+    return Object.keys(styles).reduce((rulesMap, name) => {
+      const style = styles[name]
+      rulesMap[name] = {
+        className: jss.generateClassName(JSON.stringify(style), {name}),
+        style
+      }
+      return rulesMap
+    }, {})
+  }
+
+  return {
+    css,
+    StyleSheet: {create: register}
+  }
+}
+
+export const {css, StyleSheet} = aphrodisiac(create(preset()))
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+
+process.env.VERSION = require('./package.json').version
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.VERSION': JSON.stringify(process.env.VERSION),
+    __DEV__: process.env.NODE_ENV === 'development',
+    __TEST__: process.env.NODE_ENV === 'test'
+  })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }))
+}
+
+module.exports = {
+  output: {
+    library: 'aphrodisiac',
+    libraryTarget: 'umd'
+  },
+  plugins,
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        loader: 'json-loader',
+        test: /\.json$/
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      aphroditeJss: path.join(__dirname, 'src')
+    }
+  }
+}
