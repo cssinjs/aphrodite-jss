40f28b829e48ca38c5706ac0ef81c6da7d1dc838
Oleg Slobodskoi <oleg008@gmail.com>
2016-08-15 02:30:28 +0200

    small simplifications

--- src/index.js
+++ src/index.js
@@ -1,43 +1,37 @@
 import {create} from 'jss'
 import preset from 'jss-preset-default'
 
+const isNotFalsy = val => val != null
+const getClassName = rule => rule.className
+
 export default function aphrodisiac(jss, options) {
-  const sheet = jss
-    .createStyleSheet(null, {meta: 'aphrodite-jss', ...options})
-    .attach()
-
-  const isNotEmpty = val => val != null
-
-  const getClassName = rule => rule.className
+  const sheet = jss.createStyleSheet(null, {meta: 'aphrodite-jss', ...options}).attach()
 
   function css(...rules) {
     // Filter out falsy values from the input, to allow for
     // `css(a, test && c)`
-    rules = rules.filter(isNotEmpty)
+    rules = rules.filter(isNotFalsy)
 
     if (!rules.length) return ''
 
     // A compound class name from all rules.
     const className = rules.map(getClassName).join('--')
 
-    if (sheet.getRule(className)) return className
-
-    const style = rules.reduce((res, rule) => ({...res, ...rule.style}), {})
-
-    // TODO make jss accept options here.
-    sheet.addRule(className, style, {className})
+    if (!sheet.getRule(className)) {
+      const style = rules.reduce((res, rule) => ({...res, ...rule.style}), {})
+      sheet.addRule(className, style, {className})
+    }
 
     return className
   }
 
   function register(styles) {
-    return Object.keys(styles).reduce((rulesMap, name) => {
-      const style = styles[name]
-      rulesMap[name] = {
-        className: jss.generateClassName(JSON.stringify(style), {name}),
-        style
+    return Object.keys(styles).reduce((map, name) => {
+      map[name] = {
+        className: jss.generateClassName(JSON.stringify(styles[name]), {name}),
+        style: styles[name]
       }
-      return rulesMap
+      return map
     }, {})
   }
 
